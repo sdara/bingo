5166735f7c0ed9323618c750bf2c63c1b6543b08
sdara <sdara@users.noreply.github.com>
2018-11-04 08:56:18 -0500

    First Commit

---
+++ src/bingo.configuration.js
@@ -0,0 +1,28 @@
+var BINGO = {
+	CARD: [
+		"Skateboard",
+		"4 Door Sedan",
+		"Bicycle",
+		"Vespa Scooter",
+		"Convertible",
+		"Two Door Coupe",
+		"Big Rig",
+		"Box Truck",
+		"Snow Plow",
+		"Motorcycle",
+		"SUV",
+		"Cement Mixer",
+		"Ambulance",
+		"Fire Truck",
+		"Police Car",
+		"Public Transit Bus",
+		"Garbage Truck",
+		"Flat Bed",
+		"Car Carrier",
+		"Tanker Truck",
+		"School Bus",
+		"Train",
+		"Monorail",
+		"Tow Truck"
+	]
+};
